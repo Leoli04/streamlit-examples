9e0ddbd7c2b25e1fef0b2351f0b847885bc9df5c
LeoLi04 <leoli_4@163.com>
2024-05-22 07:17:42 +0800

    fea: api-demo 增加cachedemo

--- api-demo/pages/cache_app.py
+++ api-demo/pages/cache_app.py
@@ -23,7 +23,7 @@ st.title('st.cache')
 a0 = time()
 st.subheader('Using st.cache')
 
-@st.cache(suppress_st_warning=True)
+@st.cache_data
 def load_data_a():
   df = pd.DataFrame(
     np.random.rand(2000000, 5),
